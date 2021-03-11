83f6a8526db17236978d175f9625c884cc05a17a
Richo Ha <richard.dqh@gmail.com>
2021-03-11 17:56:07 +0800

    adding next activities

--- Week-3/04-Stu_Hello-Variable/Unsolved/script.js
+++ Week-3/04-Stu_Hello-Variable/Unsolved/script.js
@@ -1,1 +1,11 @@
 // Write Your JavaScript Code Here
+
+var namme = "Ricccvcdvdfvhard";
+var pets= 03;
+var funFact= "I like to play soccer";
+
+console.log(`My name is ${namme}.`);
+console.log(`I have ${pets} pet(s).`);
+console.log(`Fun fact: ${funFact}.`);
+
+console.log("My name is "+ namme + ".");
--- Week-3/06-Stu_Primitive-Types/Unsolved/script.js
+++ Week-3/06-Stu_Primitive-Types/Unsolved/script.js
@@ -1,4 +1,4 @@
-var one = 64;
+var one = 64;       number/Booleon(true/false)/string/undefined
 var two = "656302";
 var three = false;
 var four = 64.55;
@@ -7,17 +7,39 @@ var six;
 
 // Insert comments to explain what each console log below will log to the console
 console.log(typeof one); 
+console.log(typeof 64); 
+console.log(typeof "number"); 
+
 console.log(typeof two); 
+console.log(typeof "656302"); 
+console.log(typeof "string"); 
+
 console.log(typeof three); 
+console.log(typeof false); 
+console.log(typeof "boolean"); 
+
 console.log(typeof four); 
+console.log(typeof 64.55);
+console.log(typeof "nunmber");
+
 console.log(typeof five);
+console.log(typeof false);
+console.log(typeof boolean);
+console.log(typeof "boolean");
+
 console.log(typeof six);
+console.log(typeof 23); 
+console.log(typeof number); 
+console.log(typeof "number"); 
 
 four = "Hello!";
 five = false;
 six = 23;
 
-// Insert comments to explain what each console log below will log to the console
+// Insert comments to explain what each console log below will log to the console.
 console.log(typeof four);
+// used to be a number and is replaced with a sting.
 console.log(typeof five);
+// used to be a string and is replaced with a boolean.
 console.log(typeof six);
+// used to be undefined and is replaced with a number.
