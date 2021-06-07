ca9dfe1b8aa4cccc4771cad1bd693381dfd48e66
Richo Ha <richard.dqh@gmail.com>
2021-06-07 10:14:45 +0800

    Completed activity 14

--- Week-14/14-Stu_Custom-Helpers/Unsolved/utils/helpers.js
+++ Week-14/14-Stu_Custom-Helpers/Unsolved/utils/helpers.js
@@ -9,4 +9,14 @@ module.exports = {
   },
   // TODO: Create a custom helper 'format_date' that takes in a timestamp,
   // adds five years to the date, and formats it as M/D/YYYY
+  format_date2: (inputDate) => {
+
+    var month = inputDate.getUTCMonth();
+    var day = inputDate.getUTCDate();
+    var year = inputDate.getUTCFullYear() + 5;
+    newdate = year + "/" + month + "/" + day;
+
+    return newdate;
+  },
+
 };
